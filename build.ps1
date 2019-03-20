5048de24a16c6ad2c25e99e5cc3bf2d2b1bf1f91
pw6591 <33094275+pw6591@users.noreply.github.com>
2019-03-20 21:25:34 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ $env:VERSION = $packageJson.version
 
 docker build -t pw6591/test-ci-pipeline:windows-amd64-$($env:VERSION) -f Dockerfile.windows .
 docker login -u $($env:DOCKERHUBUSERNAME) -p $($env:DOCKERHUBPASSWORD)
-docker push plossys/test-ci-pipeline:windows-amd64-$($env:VERSION)
+docker push pw6591/test-ci-pipeline:windows-amd64-$($env:VERSION)
